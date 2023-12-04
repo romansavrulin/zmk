d93accae36e82a3975b27e94b26221291c9b2a20
Roman Savrulin <romansavrulin@gmail.com>
2023-12-05 00:25:40 +0300

    Simple button merger prototype

--- app/include/zmk/matrix_transform.h
+++ app/include/zmk/matrix_transform.h
@@ -6,4 +6,10 @@
 
 #pragma once
 
+typedef struct zmk_scancode_event_registry_item {
+    sys_snode_t node;
+    uint32_t position;
+    uint32_t event_count;
+} zmk_scancode_event_registry_item_t;
+
 int32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column);
--- app/src/kscan.c
+++ app/src/kscan.c
@@ -15,6 +15,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/matrix_transform.h>
 #include <zmk/event_manager.h>
 #include <zmk/events/position_state_changed.h>
+#include <string.h>
 
 #define ZMK_KSCAN_EVENT_STATE_PRESSED 0
 #define ZMK_KSCAN_EVENT_STATE_RELEASED 1
@@ -28,6 +29,8 @@ struct zmk_kscan_event {
 struct zmk_kscan_msg_processor {
     struct k_work work;
 } msg_processor;
+
+static sys_slist_t active_keypress_registry = SYS_SLIST_STATIC_INIT(&active_keypress_registry);
 
 K_MSGQ_DEFINE(zmk_kscan_msgq, sizeof(struct zmk_kscan_event), CONFIG_ZMK_KSCAN_EVENT_QUEUE_SIZE, 4);
 
@@ -48,6 +51,7 @@ void zmk_kscan_process_msgq(struct k_work *item) {
     while (k_msgq_get(&zmk_kscan_msgq, &ev, K_NO_WAIT) == 0) {
         bool pressed = (ev.state == ZMK_KSCAN_EVENT_STATE_PRESSED);
         int32_t position = zmk_matrix_transform_row_column_to_position(ev.row, ev.column);
+        int32_t orig_position = position; //unmerged position
 
         if (position < 0) {
             LOG_WRN("Not found in transform: row: %d, col: %d, pressed: %s", ev.row, ev.column,
@@ -55,8 +59,57 @@ void zmk_kscan_process_msgq(struct k_work *item) {
             continue;
         }
 
-        LOG_DBG("Row: %d, col: %d, position: %d, pressed: %s", ev.row, ev.column, position,
+        //tbd: add merge keys config instead hardcoded values
+
+        if(position == 42)
+            position = 39; //42 is merged to 39 matrix position
+
+        LOG_INF("Row: %d, col: %d, orig_position: %d, merged_position: %d, pressed: %s", ev.row, ev.column, orig_position, position, 
                 (pressed ? "true" : "false"));
+
+        bool event_found = false;
+        bool event_exhausted = false;
+
+        zmk_scancode_event_registry_item_t *event = NULL;
+        SYS_SLIST_FOR_EACH_CONTAINER(&active_keypress_registry, event, node) {
+            if( event->position == position ) {
+                event_found = true;
+                if( pressed )
+                    event->event_count++;
+                else if (event->event_count > 0){
+                    event->event_count--;
+                }
+
+                if(event->event_count == 0)
+                    event_exhausted = true;
+
+                break;
+            }
+        }
+
+        if(event_exhausted){
+            sys_slist_find_and_remove(&active_keypress_registry, &event->node);
+            k_free(event);
+        }
+
+        if ( !event_found ){
+            event = k_malloc(sizeof(zmk_scancode_event_registry_item_t));
+            memset (event, 0, sizeof(zmk_scancode_event_registry_item_t));
+            event->position = position;
+            event->event_count = 1;
+            sys_slist_append(&active_keypress_registry, &event->node);
+        }
+
+        if(event_found && !event_exhausted){ // send only first keypress and last release event of the same position
+            LOG_INF("Merged key is already pressed. Row: %d, col: %d, "
+                "orig_position: %d, merged_position: %d, event_count: %d, pressed: %s", 
+                ev.row, ev.column, orig_position, position, event->event_count, (pressed ? "true" : "false"));
+            continue;
+        }
+
+        LOG_DBG("Raising event for row: %d, col: %d, orig_position: %d, merged_position: %d, pressed: %s", ev.row, ev.column, orig_position, position,
+                (pressed ? "true" : "false")); 
+        
         ZMK_EVENT_RAISE(new_zmk_position_state_changed(
             (struct zmk_position_state_changed){.source = ZMK_POSITION_STATE_CHANGE_SOURCE_LOCAL,
                                                 .state = pressed,
