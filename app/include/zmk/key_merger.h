68e6b194a5084b8b958368539bef7e8cfa33fb71
Roman Savrulin <romansavrulin@gmail.com>
2024-08-01 23:22:04 +0300

    Introduce configurable key merger

--- app/include/zmk/key_merger.h
+++ app/include/zmk/key_merger.h
@@ -9,4 +9,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <zephyr/devicetree.h>
+
+#if DT_HAS_CHOSEN(zmk_key_merger)
+
+#define ZMK_KEY_MERGER_NODE DT_CHOSEN(zmk_key_merger)
+#define ZMK_KEY_MERGER_MAP_LEN DT_PROP_LEN(ZMK_KEY_MERGER_NODE, map)
+
+#endif /* DT_HAS_CHOSEN(zmk_key_merger) */
+
 bool zmk_key_merger_consume_event(uint32_t position, bool pressed);
--- app/src/key_merger.c
+++ app/src/key_merger.c
@@ -4,17 +4,35 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <zmk/key_merger.h>
+
+#ifdef ZMK_KEY_MERGER_NODE
+
+/* the key_merger node in the device tree is a list of (from,to) pairs that
+ * represent a keys that we want to act like a single mechanical button.  
+ * You can also treat two merged keys like two keys connected electrically
+ * in parallel.
+ *
+ * We do this by replacing key index using from->to rule and counting 
+ * keypress/release events of resulting "to" key. Only first keypress and 
+ * last key release of the pair is sent to listeners.
+ * We want to be able to detect and skip the condition when keys not assigned 
+ * to merger is pressed and pass events from it as is.
+ */
+
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/bluetooth/addr.h>
 #include <zephyr/drivers/kscan.h>
 #include <zephyr/logging/log.h>
 
+// transform functions
+#include <dt-bindings/zmk/matrix_transform.h>
+
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/matrix_transform.h>
 #include <zmk/event_manager.h>
-#include <zmk/key_merger.h>
 #include <zmk/events/position_state_changed.h>
 #include <string.h>
 
@@ -27,16 +45,39 @@ typedef struct zmk_scancode_event_registry_item {
 
 static sys_slist_t active_keypress_registry = SYS_SLIST_STATIC_INIT(&active_keypress_registry);
 
+static uint32_t transform[] = DT_PROP(ZMK_KEY_MERGER_NODE, map);
+
+#endif /* ZMK_KEY_MERGER_NODE */
+
 bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
+#ifdef ZMK_KEY_MERGER_NODE
+
     int32_t orig_position = position; // unmerged position
+    bool found = false;
 
-    // tbd: add merge keys config instead hardcoded values
+    if (ARRAY_SIZE(transform) == 0)
+        return false;
 
-    if (position == 42)
-        position = 39; // merge spaces between both sides
+    for (int i = 0; i < (ARRAY_SIZE(transform)); ++i) {
+
+        uint32_t from = (transform[i] >> 8) & 0xFF;
+        uint32_t to = transform[i] & 0xFF;
+
+        if (position == to)
+            found = true; // Event is related to configured merge key
+
+        if (position == from) {
+            position = to;
+            found = true;
+            LOG_INF("Merging key %d to %d", from, to);
+        }
+    }
+
+    if (!found)
+        return false; // Skip list manipulation for unrelated keys
 
     LOG_INF("Orig_position: %d, merged_position: %d, pressed: %s", orig_position, position,
-            (pressed ? "true" : "false"));
+                (pressed ? "true" : "false"));
 
     bool event_found = false;
     bool event_exhausted = false;
@@ -81,6 +122,6 @@ bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
 
     LOG_DBG("Raising event for orig_position: %d, merged_position: %d, pressed: %s", orig_position,
             position, (pressed ? "true" : "false"));
-
+#endif /* ZMK_KEY_MERGER_NODE */
     return false;
 }
