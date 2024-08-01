cca2ccbf890304b6fdcaa027d141bbc97146f2fe
Roman Savrulin <romansavrulin@gmail.com>
2024-08-01 20:29:37 +0300

    Prettify

--- app/src/key_merger.c
+++ app/src/key_merger.c
@@ -28,12 +28,12 @@ typedef struct zmk_scancode_event_registry_item {
 static sys_slist_t active_keypress_registry = SYS_SLIST_STATIC_INIT(&active_keypress_registry);
 
 bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
-    int32_t orig_position = position; //unmerged position
+    int32_t orig_position = position; // unmerged position
 
     // tbd: add merge keys config instead hardcoded values
 
     if (position == 42)
-        position = 39; //merge spaces between both sides
+        position = 39; // merge spaces between both sides
 
     LOG_INF("Orig_position: %d, merged_position: %d, pressed: %s", orig_position, position,
             (pressed ? "true" : "false"));
@@ -43,11 +43,11 @@ bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
 
     zmk_scancode_event_registry_item_t *event = NULL;
     SYS_SLIST_FOR_EACH_CONTAINER(&active_keypress_registry, event, node) {
-        if( event->position == position ) {
+        if (event->position == position ) {
             event_found = true;
-            if( pressed )
+            if (pressed)
                 event->event_count++;
-            else if (event->event_count > 0){
+            else if (event->event_count > 0) {
                 event->event_count--;
             }
 
@@ -58,12 +58,12 @@ bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
         }
     }
 
-    if(event_exhausted){
+    if (event_exhausted) {
         sys_slist_find_and_remove(&active_keypress_registry, &event->node);
         k_free(event);
     }
 
-    if ( !event_found ){
+    if (!event_found) {
         event = k_malloc(sizeof(zmk_scancode_event_registry_item_t));
         memset (event, 0, sizeof(zmk_scancode_event_registry_item_t));
         event->position = position;
@@ -71,15 +71,16 @@ bool zmk_key_merger_consume_event(uint32_t position, bool pressed) {
         sys_slist_append(&active_keypress_registry, &event->node);
     }
 
-    if (event_found && !event_exhausted){ // send only first keypress and last release event of the same position
+    if (event_found && 
+        !event_exhausted) { // send only first keypress and last release event of the same position
         LOG_INF("Merged key is already pressed. Consuming event"
             "orig_position: %d, merged_position: %d, event_count: %d, pressed: %s", 
             orig_position, position, event->event_count, (pressed ? "true" : "false"));
         return true;
     }
 
-    LOG_DBG("Raising event for orig_position: %d, merged_position: %d, pressed: %s", orig_position, position,
-            (pressed ? "true" : "false"));
+    LOG_DBG("Raising event for orig_position: %d, merged_position: %d, pressed: %s", orig_position, 
+        position, (pressed ? "true" : "false"));
 
     return false;
 }
